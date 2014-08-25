4fe5f2816ba792504ef3c547423a41ef1865a6c7
Alexey Chulochnikov <chulochnikov@gmail.com>
2014-08-25 15:31:33 +0300

    Initial Commit

---
+++ KitchenSwift/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  KitchenSwift
+//
+//  Created by Alexey Chulochnikov on 25.08.14.
+//  Copyright (c) 2014 Alexey Chulochnikov. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+                            
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
