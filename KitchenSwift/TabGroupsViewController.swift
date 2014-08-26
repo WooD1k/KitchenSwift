cca78bfd021bd4a91278b4e0a8c915c6b92c93b8
Alexey Chulochnikov <chulochnikov@gmail.com>
2014-08-26 19:21:25 +0300

    TabGroupsVC added to the project

---
+++ KitchenSwift/TabGroupsViewController.swift
@@ -0,0 +1,14 @@
+//
+//  tabGroups.swift
+//  KitchenSwift
+//
+//  Created by Alexey Chulochnikov on 26.08.14.
+//  Copyright (c) 2014 Alexey Chulochnikov. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TabGroupsViewController: UIViewController {
+	
+}
